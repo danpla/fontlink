3aedfb377e356012d274e4c088701dd8c54883f8
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2015-08-07 18:42:42 +0300

    Simplified unique_name.

--- fontlink/utils.py
+++ fontlink/utils.py
@@ -16,23 +16,23 @@ def ext_to_glob(extension):
 def unique_name(name, all_names):
     '''Make the name unique by appending "#n" at the end.'''
 
-    if name in all_names:
-        head, sep, tail = name.rpartition('#')
-        if sep:
-            try:
-                i = int(tail)
-                name = head.rstrip()
-            except ValueError:
-                pass
-        else:
-            i = 1
+    if name not in all_names:
+        return name
 
-        similar_names = set(n for n in all_names if name in n)
-        while True:
-            new_name = '{} #{}'.format(name, i)
-            if new_name not in similar_names:
-                name = new_name
-                break
+    head, sep, tail = name.rpartition('#')
+    if sep:
+        try:
+            i = int(tail)
+            name = head.rstrip()
+        except ValueError:
+            pass
+    else:
+        i = 1
+
+    similar_names = set(n for n in all_names if n.startswith(name))
+    while True:
+        new_name = '{} #{}'.format(name, i)
+        if new_name in similar_names:
             i += 1
-
-    return name
+        else:
+            return new_name
